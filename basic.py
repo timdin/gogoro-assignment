3b6670438a0b0a30adb68993f9a81430658f4ff3
Tim Pei <timpei123@gmail.com>
2024-08-01 14:27:12 +0800

    [impl] update the basic solution with exhaustive retrival and value handling

--- basic.py
+++ basic.py
@@ -23,11 +23,31 @@ sortedNames = list(map(lambda x: x["title"], films))
 print(f"Please list all the film names and sort the name by episode_id\n It was: {sortedNames}")
 
 # 3. Please find out all vehicles which max_atmosphering_speed is over 1000.
-res = requests.get(VEHICLE_URL, timeout = DEFAULT_TIMEOUT)
-body = res.json()
-vehicles = body["results"]
-qualifiedVehicles = list(filter(lambda x : x["name"] if int(x
-["max_atmosphering_speed"]) > 1000 else None, vehicles))
-filteredNames = list(map(lambda x: x["name"], qualifiedVehicles))
+# exhaustive retrival of vehicle
+vehicles = []
+next_url = VEHICLE_URL
+while next_url:
+    res = requests.get(next_url, timeout = DEFAULT_TIMEOUT)
+    body = res.json()
+    vehicles.extend(body["results"])
+    next_url = body["next"]
+
+# since there are non-digit strings exists in the response, avoid the lambda solution
+# qualifiedVehicles = list(
+#     filter(
+#         lambda x : x["name"] if int(x["max_atmosphering_speed"]) > 1000 else None, vehicles
+#         )
+#     )
+# filteredNames = list(map(lambda x: x["name"], qualifiedVehicles))
+
+filteredNames = []
+for vehicle in vehicles:
+    try:
+        if int(vehicle["max_atmosphering_speed"])>=1000:
+            filteredNames.append(vehicle["name"])
+    except ValueError:
+        # only catch value error here, since we are only suppressing the non-digit strings casting
+        continue
+
 print(f"""Please find out all vehicles which max_atmosphering_speed is over 1000.
  It was: {filteredNames}""")
